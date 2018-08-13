dd7796657a879f70841578b49a23ec1285b4724f
ljliu <lljstar@163.com>
2018-08-13 15:08:59 +0800

    init

---
+++ webapp-server1/src/main/java/com/example/webapp/server/controller/ConfigController.java
@@ -0,0 +1,25 @@
+package com.example.webapp.server.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/config")
+public class ConfigController {
+    @Autowired
+    Environment environment;
+
+    @RequestMapping(value = "/myname")
+    public String getInfo(){
+        return this.environment.getProperty("spring.myname");
+    }
+
+    @RequestMapping(value = "/show")
+    public String[] all(){
+        return  environment.getActiveProfiles();
+    }
+
+
+}
