e064dc1a9a8e9e7a0523d4a5a0c0cc6bb32fd199
ljliu <lljstar@163.com>
2018-08-15 18:02:35 +0800

    init

--- config-server2/src/main/java/com/example/config/server/WebSecurityConfig.java
+++ config-server2/src/main/java/com/example/config/server/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.config.server;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,9 +9,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .anyRequest().authenticated()
-                .and().httpBasic();
+        http.csrf().disable();
+
+        http.requestMatcher(EndpointRequest.toAnyEndpoint()).authorizeRequests()
+                .anyRequest().hasRole("ENDPOINT_ADMIN")
+                .and()
+                .httpBasic();
     }
 }
